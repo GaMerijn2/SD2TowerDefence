4e88b535920599d11b79358e7c4aece39420fd83
merijn florie <merijn@florie.nl>
2023-10-19 13:18:17 +0200

    .

--- TD/Assets/Menus.cs
+++ TD/Assets/Menus.cs
@@ -5,6 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
+    [SerializeField] private bool invokeBool = true;
+    [SerializeField] private float timeSpeed = 9;
+    
+    public void PauseGame()
+    {
+        for (int i = 0; i < timeSpeed; i++)
+        {
+            Time.timeScale -= 0.1f;
+            Debug.Log(Time.timeScale);
+            if (Time.timeScale <= 0.1)
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        for (int i = 0; i < timeSpeed; i++)
+        {
+            Time.timeScale += 0.1f;
+            Debug.Log(Time.timeScale);
+            if (Time.timeScale >= 1)
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("GameScene");
---
+++ TD/Assets/eNEMYwAVES.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class eNEMYwAVES 
+{
+    // Start is called before the first frame update
+    public List<GameObject>[] GameObjects;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
